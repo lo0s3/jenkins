8dc6d1a847390d8c6fe0c433b04179fefa644119
lo0s3 <33362536+lo0s3@users.noreply.github.com>
2020-06-15 13:14:58 -0700

    Update its_groovy.py

--- its_groovy.py
+++ its_groovy.py
@@ -6,27 +6,27 @@ from gevent.pool import Pool
 gevent.monkey.patch_socket()
 requests.packages.urllib3.disable_warnings()
 
-parser = argparse.ArgumentParser(description="Multithreaded stuff")
-parser.add_argument('-i', dest='infile', required=True, metavar="FILE")
+parser = argparse.ArgumentParser(description="Jenkins Open Auth Script Console Scanner")
+parser.add_argument('-i', dest='hosts', required=True, metavar="HOSTNAMES", help="hostname list file")
 #parser.add_argument('-o', dest='outdir', required=False, metavar="FILE", type=str, default="output")
-parser.add_argument('-t', dest="threads", required=False, type=int, default=16)
+parser.add_argument('-t', dest="threads", required=False, type=int, default=16, metavar="THREADS", help="number of threads to use")
 
 args = parser.parse_args()
 
 
-def check_urls(urls, threads):
-    def fetch(url):
+def check_hosts(hosts, threads):
+    def fetch(host):
         try:
-            response = requests.request("GET", "http://" + url + ":8080/script", timeout=4, verify=False)
-            #print("[*] Querying: " + "http://" + url + ":8080/script")
+            response = requests.request("GET", "http://" + host + ":8080/script", timeout=4, verify=False)
+            #print("[*] Querying: " + "http://" + host + ":8080/script")
             data = fromstring(response.content)
             title = data.findtext('.//title')
             service = str(title)
 
             # <title> is jenkins
             if service == 'Jenkins':
-                print("[*] Found " + service + " at: " + url)
-            #with open(outdir + "/" + url, "w+") as fp:
+                print("[*] Found " + service + " at: " + host)
+            #with open(outdir + "/" + host, "w+") as fp:
             #    fp.write(response.text.encode("utf-8"))
 
         except Exception as e:
@@ -34,11 +34,11 @@ def check_urls(urls, threads):
             print(e)
             pass
 
-    N = len(urls)
+    N = len(hosts)
     pool = Pool(nthreads)
-    for index, url in enumerate(urls):
-        #print(url)
-        pool.spawn(fetch, url)
+    for index, host in enumerate(hosts):
+        #print(host)
+        pool.spawn(fetch, host)
     pool.join()
 
 
@@ -48,10 +48,10 @@ def check_urls(urls, threads):
 
 
 if __name__ == "__main__":
-    filename = args.infile
+    filename = args.hosts
     #outdir   = args.outdir
     nthreads = args.threads
 
     #check_dir(outdir)
-    ips = [line.strip() for line in open(filename, ).readlines()]
-    check_urls(ips, nthreads)
+    hostnames = [line.strip() for line in open(filename, ).readlines()]
+    check_hosts(hostnames, nthreads)
